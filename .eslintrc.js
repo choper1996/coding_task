a40dffa67796c46d5d729a71b4aab02c3fd3452d
m.pakhomov <m.pakhomov@kokoc.tech>
2024-01-29 14:43:34 +0300

    chore(): инициализация husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
         // 'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
         // 'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
         'no-param-reassign': 'off',
+        'react/no-unused-prop-types': 'off',
     },
     globals: {
         __IS_DEV__: true,
